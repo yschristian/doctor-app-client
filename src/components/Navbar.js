4a9fb21ee6897db8fa6fccda624e9262cff85d13
yschristian <yubahwesc@gmail.com>
2023-06-17 09:24:19 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import "./navbar.css"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import { Badge, message } from "antd";
+import { useSelector } from 'react-redux';
 
 function Navbar() {
   const token = localStorage.getItem("token")
   const [isMenuToggled, setIsMenuToggled] = useState(true)
   const navigate = useNavigate();
+  const { user } = useSelector((state) => state.user);
+  const location = useLocation();
   // logout funtion
   const handleLogout = () => {
     localStorage.clear();
@@ -17,19 +20,33 @@ function Navbar() {
     <header id="header" class="fixed-top">
       <div class="container d-flex align-items-center">
 
-        <h1 class="logo me-auto"><Link to="/"><a >Dream Medical H.</a></Link></h1>
+        <h1 class="logo me-auto"><Link to="/"><a >Dream M Center.</a></Link></h1>
 
         <nav id="navbar"  className={isMenuToggled ? 'navbar order-last order-lg-0' : "navbar-mobile"}>
           <ul>
-            <li><a class="nav-link scrollto active" href="/">Home</a></li>
             {
               token ? 
              <>
-              <li style={{textDecoration:"none"}}><a class="nav-link " ><Link style={{textDecoration:"none"}} to="/dashboard">Dasboard</Link></a></li>
-              <li style={{textDecoration:"none"}}><a class="nav-link " ><Link onClick={handleLogout} style={{textDecoration:"none"}} >Logout</Link></a></li>
+              {/* <li style={{textDecoration:"none"}}><a class="nav-link " ><Link style={{textDecoration:"none"}} to="/dashboard">Dasboard</Link></a></li> */}
+              {/* <li style={{textDecoration:"none"}}><a class="nav-link " ><Link onClick={handleLogout} style={{textDecoration:"none"}} >Logout</Link></a></li> */}
+              <div className="header1">
+              <div className="header-content" style={{ cursor: "pointer" }}>
+                <Badge
+                  count={user && user.notifcation.length}
+                  onClick={() => {
+                    navigate("/notification");
+                  }}
+                >
+                  <i class="fa-solid fa-bell"></i>
+                </Badge>
+
+                <Link to="/dashboard">{user?.name}</Link>
+              </div>
+            </div>
              </>
               :
                 <>
+                  <li><a class="nav-link scrollto active" href="/">Home</a></li>
                   <li style={{textDecoration:"none"}}><a class="nav-link " ><Link style={{ textDecoration: "none" }} to="/login">Login</Link></a></li>
                   <li style={{textDecoration:"none"}}><a class="nav-link " ><Link style={{ textDecoration: "none" }} to="/register">Register</Link></a></li>
                 </>
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -6,8 +6,8 @@ const Topbar = () => {
     <div id="topbar" class="d-flex align-items-center fixed-top">
     <div class="container d-flex justify-content-between">
       <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope"></i> <a href="mailto:yschristian7@gmail.com">yschristian7@gmail.com</a>
-        <i class="bi bi-phone"></i><a href="tel:+250784381529">+250784381529</a>
+        <i class="bi bi-envelope"></i> <a href="mailto:medicaldreamcenter@gmail.com">medicaldreamcenter@gmail.com</a>
+        <i class="bi bi-phone"></i><a href="tel:+250788875589">+250788875589</a>
     </div>
       <div class="d-none d-lg-flex social-links align-items-center">
         <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
