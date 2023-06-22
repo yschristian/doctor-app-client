979602ba9cfdf7367a2701af901a964d697032bf
yschristian <yubahwesc@gmail.com>
2023-06-22 13:25:15 +0200

    apply doctor

--- src/pages/ApplyDoctor.js
+++ src/pages/ApplyDoctor.js
@@ -16,7 +16,7 @@ const ApplyDoctor = () => {
     try {
       dispatch(showLoading());
       const res = await axios.post(
-        "/api/v1/user/apply-doctor",
+        "https://doctor-app-server.onrender.com/api/v1/user/apply-doctor",
         {
           ...values,
           userId: user._id,
