ebc477cfa7c2ab58aaeaa4de457e910f97661dd3
yschristian <yubahwesc@gmail.com>
2023-08-13 00:34:58 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
             <h4>Useful Links</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="/">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="/">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/about">About us</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="/">Services</a></li>
               {/* <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li> */}
