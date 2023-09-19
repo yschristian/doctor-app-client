56f09665603b0442805ce59e920b98cabffdc106
Maker Studios <mashamipaccy04@gmail.com>
2023-09-19 22:26:41 +0200

    feat: changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
 import "./navbar.css"
-import { Link } from 'react-router-dom'
 
 function Main() {
   const token = localStorage.getItem("token")
@@ -8,8 +8,17 @@ function Main() {
     <div id="hero" class="d-flex align-items-center">
       <div class="container">
         <h1>Welcome to Dream Medical Center.</h1>
-        <h2>We are here to help you, to make appointment with <br></br> doctors of different Clinics and Hospitals</h2>
-       {token ? "" : <Link to="/login"><a class="btn-get-started scrollto">Make An Appointments</a></Link>}
+        <h2>
+          We are here to help you, to make appointment with <br></br> different
+          doctors in our hospital.
+        </h2>
+        {token ? (
+          ""
+        ) : (
+          <Link to="/login">
+            <a class="btn-get-started scrollto">Make An Appointments</a>
+          </Link>
+        )}
       </div>
     </div>
   )
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,56 +1,93 @@
-import React from 'react'
+import React from "react"
 
 const Services = () => {
-    return (
-        <div class="container" style={{ marginTop: "250px" }}>
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="section-title">
-                        <span>Why choose us?</span>
-                        <h2>Offer for you</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="chooseus__item">
-                        <div class="icon d-flex justify-content-center" >
-                            <i class="fa-solid fa-microscope" style={{ fontSize: "50px" }}></i>
-                        </div>
-                        <h5 className='fw-bold text-center py-1' >Advanced equipment</h5>
-                        <p>Lorem ipsum amet, consectetur adipiscing elit, sed do eiusmod tempor cididunt facilisis.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="chooseus__item">
-                        <div class="icon d-flex justify-content-center" >
-                            <i class="fa-solid fa-briefcase-medical" style={{ fontSize: "50px" }}></i>
-                        </div>
-                        <h5 class="text-center fw-bold py-1">Qualified doctors</h5>
-                        <p>Lorem ipsum amet, consectetur adipiscing elit, sed do eiusmod tempor cididunt facilisis.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="chooseus__item">
-                        <div class="icon d-flex justify-content-center" >
-                            <i class="fa-solid fa-book-medical" style={{ fontSize: "50px" }}></i>
-                        </div>
-                        <h5 class="text-center fw-bold py-1">Certified services</h5>
-                        <p>Lorem ipsum amet, consectetur adipiscing elit, sed do eiusmod tempor cididunt facilisis.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="chooseus__item">
-                        <div class="icon d-flex justify-content-center" >
-                        <i class="fa-solid fa-truck-medical" style={{ fontSize: "50px" }}></i>
-                        </div>
-                        <h5 class="text-center fw-bold py-1">Emergency care</h5>
-                        <p>Lorem ipsum amet, consectetur adipiscing elit, sed do eiusmod tempor cididunt facilisis.</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div class="container" style={{ marginTop: "250px" }}>
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <div class="section-title">
+            <span>Why choose us?</span>
+            <h2>Offer for you</h2>
+          </div>
         </div>
-    )
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="chooseus__item">
+            <div class="icon d-flex justify-content-center">
+              <i
+                class="fa-solid fa-microscope"
+                style={{ fontSize: "50px" }}
+              ></i>
+            </div>
+            <h5 className="fw-bold text-center py-1">Advanced equipment</h5>
+            <p>
+              Advanced equipment in doctor appointment services refers to
+              state-of-the-art medical technology and tools integrated into the
+              patient care process. These cutting-edge devices enhance the
+              accuracy of diagnoses, improve treatment outcomes, and streamline
+              the overall healthcare experience.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="chooseus__item">
+            <div class="icon d-flex justify-content-center">
+              <i
+                class="fa-solid fa-briefcase-medical"
+                style={{ fontSize: "50px" }}
+              ></i>
+            </div>
+            <h5 class="text-center fw-bold py-1">Qualified doctors</h5>
+            <p>
+              Qualified doctors are highly trained and experienced medical
+              professionals who have completed rigorous educational programs and
+              practical training in the field of medicine. They hold medical
+              degrees and licenses, demonstrating their expertise and competence
+              in diagnosing, treating, and managing a wide range of medical
+              conditions.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="chooseus__item">
+            <div class="icon d-flex justify-content-center">
+              <i
+                class="fa-solid fa-book-medical"
+                style={{ fontSize: "50px" }}
+              ></i>
+            </div>
+            <h5 class="text-center fw-bold py-1">Certified services</h5>
+            <p>
+              These certifications serve as a mark of quality, reliability, and
+              compliance with specific criteria or requirements. When you choose
+              certified services, you can have confidence in their ability to
+              deliver exceptional results and maintain a high level of
+              professionalism.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="chooseus__item">
+            <div class="icon d-flex justify-content-center">
+              <i
+                class="fa-solid fa-truck-medical"
+                style={{ fontSize: "50px" }}
+              ></i>
+            </div>
+            <h5 class="text-center fw-bold py-1">Emergency care</h5>
+            <p>
+              Emergency care refers to the immediate and specialized medical
+              attention provided to individuals facing sudden and critical
+              health crises. These situations can encompass a wide range of
+              medical emergencies, including severe injuries, life-threatening
+              illnesses, and acute medical conditions.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Services
